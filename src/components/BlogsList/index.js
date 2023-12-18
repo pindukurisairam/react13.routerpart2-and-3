1b8fd8cbfda26cda29e974f1607b4d3c10e4b403
pindukurisairam <sairamsai524@gmail.com>
2023-12-18 17:36:32 +0000

    solutin

---
+++ src/components/BlogsList/index.js
@@ -0,0 +1,53 @@
+import {Component} from 'react'
+
+import Loader from 'react-loader-spinner'
+
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+
+import BlogItem from '../BlogItem'
+
+import './index.css'
+
+class BlogsList extends Component {
+  state = {
+    blogsData: [],
+    isLoading: true, {/*If we give true the spinner will come in output while loading and we give false means directly the spinner is not come*/}
+  }
+
+  componentDidMount() {
+    this.getBlogsData()
+  }
+
+  getBlogsData = async () => {
+    const response = await fetch('https://apis.ccbp.in/blogs')
+    const data = await response.json()
+    console.log(data)
+    const updatedData = data.map(eachItem => ({
+      id: eachItem.id,
+      title: eachItem.title,
+      imageUrl: eachItem.image_url,
+      avatarUrl: eachItem.avatar_url,
+      author: eachItem.author,
+      topic: eachItem.topic,
+    }))
+    this.setState({blogsData: updatedData})
+    console.log(updatedData)
+  }
+
+  render() {
+    const {blogsData} = this.state
+    const {isLoading} = this.state
+
+    return (
+      <div className="blog-list-container">
+        {isLoading ? (
+          <Loader type="TailSpin" color="#00BFFF" height={50} width={50} />
+        ) : (
+          blogsData.map(item => <BlogItem blogData={item} key={item.id} />)
+        )}
+      </div>
+    )
+  }
+}
+
+export default BlogsList
